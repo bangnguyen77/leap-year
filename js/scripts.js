365ff32d195ae288c9adeed0a450260f918a710d
Bang Nguyen and Wolfgang Warneke <wolfgang@>
2016-06-08 09:16:35 -0700

    find a bug about input like this (2004ahaah), it returns (the year 2004ahaaha is not a leap year)

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     var leapYear = function(yearInput) {
       if (!year) {
-        $("#result h1").text('this was not a valid number, please try again');
+        return '<h1>This was not a valid year. Please try again.</h1>';
       } else if ( (yearInput % 4 === 0) ) {
-        return true;
+        return "The year " + yearInput + " is a leap year.";
       } else {
-        return false;
+        return "The year " + yearInput + " is not a leap year.";
       }
     }
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result h1").text(result);
+    $("#result").html("<h1>" + result + "</h1>");
     event.preventDefault();
   });
 });
