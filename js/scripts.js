838fbdf8f8fe75d9ba2aae7faaec27e996451e69
Bang Nguyen and Wolfgang Warneke <wolfgang@>
2016-06-08 09:52:49 -0700

    add comment to explain conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     var leapYear = function(yearInput) {
-      if (!yearInput || !Number($("input#year").val()) || (yearFloatTest % 1 !== 0) ) {
+      // in "if" !yearInput filters strings because parseInt returns NaN, which is "falsy"
+      // if we have strings after the number (eg "2004era") then !yearInput is false, because parseInt will return 2004 which IS true, so not (!) will advance to the next condition.  Then variable 'yearNumTest' is evaluated which uses Number() which returns Nan if there are any non-number characters.  It returns Nan which is false, so !false is true.
+      // if we have a float (eg 2004.123), it fails the first two conditions and then is tested for NOT having a remainder of 0 with modulo 1.  Any whole number will have no remainder and any float will have a remainder of 0.'decimals' (eg 0.123)
+      if (!yearInput || !yearNumTest || (yearNumTest % 1 !== 0) ) {
         return '<h1>This was not a valid year. Please try again.</h1>';
       } else if ( (yearInput % 4 === 0) ) {
         return "The year " + yearInput + " is a leap year.";
@@ -9,7 +12,7 @@ $(document).ready(function() {
         return "The year " + yearInput + " is not a leap year.";
       }
     }
-    var yearFloatTest = Number($("input#year").val());
+    var yearNumTest = Number($("input#year").val());
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").html("<h1>" + result + "</h1>");
