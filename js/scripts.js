228faa2cd351f95b5c47848b6ed463cd8f8635b9
Bang Nguyen and Wolfgang Warneke <wolfgang@>
2016-06-08 09:36:03 -0700

    add conditions to sort out invalid year input

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     var leapYear = function(yearInput) {
-      if (!year) {
+      if (!yearInput || !Number($("input#year").val()) || (yearFloatTest % 1 !== 0) ) {
         return '<h1>This was not a valid year. Please try again.</h1>';
       } else if ( (yearInput % 4 === 0) ) {
         return "The year " + yearInput + " is a leap year.";
@@ -9,6 +9,7 @@ $(document).ready(function() {
         return "The year " + yearInput + " is not a leap year.";
       }
     }
+    var yearFloatTest = Number($("input#year").val());
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").html("<h1>" + result + "</h1>");
